2154e5ba60e77a1bcba8f1a40c3b2fe7fa6934ad
Weinan Shi <shi.wein@northeastern.edu>
2023-11-02 17:08:48 -0700

    update

--- src/Kanbas/Courses/Assignments/AssignmentList.js
+++ src/Kanbas/Courses/Assignments/AssignmentList.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+// import { useParams, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import {
     deleteAssignment,
