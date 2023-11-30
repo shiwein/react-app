656e946d2d80c9134fc428d0b18c953f416a8118
Weinan Shi <shi.wein@northeastern.edu>
2023-11-30 13:08:34 -0800

    status

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 import {
   addModule,
   deleteModule,
@@ -20,8 +20,8 @@ function ModuleList() {
     client.findModulesForCourse(courseId)
       .then((modules) =>
         dispatch(setModules(modules))
-    );
-  }, [courseId]);
+      );
+  }, [courseId, dispatch]);
   const handleAddModule = () => {
     client.createModule(courseId, module).then((module) => {
       dispatch(addModule(module));
@@ -33,7 +33,7 @@ function ModuleList() {
     });
   };
   const handleUpdateModule = async () => {
-    const status = await client.updateModule(module);
+    await client.updateModule(module);
     dispatch(updateModule(module));
   };
 
@@ -41,42 +41,42 @@ function ModuleList() {
 
   return (
     <ul className="list-group mt-2 me-3">
-      <li className="list-group-item"> 
-      <div className="row">
-        <div className="col-7">
-          <input value={module.name}
-            onChange={(e) => dispatch(setModule({...module, name: e.target.value}))}
-          className="form-control mt-1 mb-1"/>
-          <textarea value={module.description}
-            onChange={(e) => dispatch(setModule({ ...module, description: e.target.value }))
-          } className="form-control mb-1"/>
+      <li className="list-group-item">
+        <div className="row">
+          <div className="col-7">
+            <input value={module.name}
+              onChange={(e) => dispatch(setModule({ ...module, name: e.target.value }))}
+              className="form-control mt-1 mb-1" />
+            <textarea value={module.description}
+              onChange={(e) => dispatch(setModule({ ...module, description: e.target.value }))
+              } className="form-control mb-1" />
+          </div>
+          <div className="col">
+            <button onClick={handleUpdateModule} className="btn  btn-primary me-2 float-end">
+              Update
+            </button>
+            <button onClick={handleAddModule} className="btn  btn-success float-end me-2">
+              Add
+            </button>
+          </div>
         </div>
-        <div className="col">
-          <button onClick={handleUpdateModule} className="btn  btn-primary me-2 float-end">
-            Update
-          </button> 
-          <button onClick={handleAddModule} className="btn  btn-success float-end me-2">
-            Add
-          </button>
-        </div>
-      </div>
       </li>
       {
-       modules
-         .filter((module) => module.course === courseId)
-         .map((module, index) => (
-           <li key={index} className="list-group-item" style={{borderRadius:"0px"}}>
-             <button onClick={() => dispatch(setModule(module))} className="btn  btn-success float-end me-2">
+        modules
+          .filter((module) => module.course === courseId)
+          .map((module, index) => (
+            <li key={index} className="list-group-item" style={{ borderRadius: "0px" }}>
+              <button onClick={() => dispatch(setModule(module))} className="btn  btn-success float-end me-2">
                 Edit
               </button>
-             <button onClick={() => handleDeleteModule(module._id)} className="btn  btn-danger float-end me-2">
+              <button onClick={() => handleDeleteModule(module._id)} className="btn  btn-danger float-end me-2">
                 Delete
               </button>
-             <h3>{module.name}</h3>
-             <p className="fs-5">{module.description}</p>
-             <p>{module._id}</p>
-           </li>
-      ))
+              <h3>{module.name}</h3>
+              <p className="fs-5">{module.description}</p>
+              <p>{module._id}</p>
+            </li>
+          ))
       }
     </ul>
   );
